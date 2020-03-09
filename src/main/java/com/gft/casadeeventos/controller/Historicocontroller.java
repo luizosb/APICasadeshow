31033efc71ca0d4abc4a16fef6baca195e4e6fe9
luizosb <luiz-osb@hotmail.com>
2020-03-09 09:46:12 -0300

    Heroku Deploy

--- src/main/java/com/gft/casadeeventos/controller/Historicocontroller.java
+++ src/main/java/com/gft/casadeeventos/controller/Historicocontroller.java
@@ -3,8 +3,10 @@ package com.gft.casadeeventos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gft.casadeeventos.model.Ingresso;
 import com.gft.casadeeventos.repository.Ingressos;
@@ -31,5 +33,11 @@ public class Historicocontroller {
 		
 	}
 	
+	@RequestMapping(value = "{id}")
+	public String excluir(@PathVariable Long id, RedirectAttributes attributes) {
+		ingress.deleteById(id);
+		attributes.addFlashAttribute("mensagem", "Compra excluída com sucesso!");
+		return "redirect:/historico";
+	}
 	
 }
